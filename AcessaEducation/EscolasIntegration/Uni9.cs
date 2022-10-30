77fb142a8888bfbe1923bcb2649c58bc9f923167
Felipe Muniz <felipemuniz_hub@outlook.com>
2022-10-30 15:43:59 -0300

    Inicio Implementação da Central do Aluno

--- AcessaEducation/EscolasIntegration/Uni9.cs
+++ AcessaEducation/EscolasIntegration/Uni9.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace AcessaEducation.EscolasIntegration
 {
@@ -13,27 +15,60 @@ namespace AcessaEducation.EscolasIntegration
         private string _loginURI = "https://aluno.uninove.br/seu/CENTRAL/aluno/";
         private Page _currentPage;
         private Browser _browser;
+        private string _ra;
+        private string _senha;
         public async Task AcessaUni9()
         {
             Console.WriteLine("=============================== Digite seu Ra =====================================");
-            string ra = Console.ReadLine();
+            _ra = Console.ReadLine();
             Console.WriteLine("============================== Digite Sua Senha ====================================");
-            string senha = Console.ReadLine();
+            _senha = Console.ReadLine();
 
             #region Login
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
             _browser = await Puppeteer.LaunchAsync(new LaunchOptions
             {
                 Headless = false
             });
 
-            await _currentPage.GoToAsync(_loginURI);
-            await _currentPage.WaitForSelectorAsync("#user");
+            _currentPage = await _browser.NewPageAsync();
+            await _currentPage.GoToAsync("https://aluno.uninove.br/seu/CENTRAL/aluno/");
+            await _currentPage.WaitForSelectorAsync("#BtnConf");
             await _currentPage.FocusAsync("#user");
-            await _currentPage.Keyboard.TypeAsync(ra);
+            await _currentPage.Keyboard.TypeAsync(_ra);
             await _currentPage.FocusAsync("#Password");
-            await _currentPage.Keyboard.TypeAsync(senha);
-
+            await _currentPage.Keyboard.TypeAsync(_senha);
+            Thread.Sleep(1000);
+            await _currentPage.ClickAsync("#BtnConf");
+            await _currentPage.WaitForSelectorAsync(".bem-vindo");
+            Console.WriteLine("Acesso Concluido com Sucesso");
+            Console.WriteLine("========================================================================================");
             #endregion
+
+            #region Serviços
+            Console.WriteLine("============================ Selecione o Serviço ====================================");
+            Console.WriteLine("=====================================================================================");
+            Console.WriteLine("1- Central do Aluno");
+            Console.WriteLine("=====================================================================================");
+
+            int services = int.Parse(Console.ReadLine());
+            Console.WriteLine("=====================================================================================");
+
+            switch (services)
+            {
+                case 1:
+                    await _currentPage.ClickAsync("#btn-acessa-central");
+                    await _currentPage.WaitForSelectorAsync("#content-main > div > div:nth-child(1) > div > div.m-portlet__head > div > div > h3");
+                    await GetSentralAluno();
+                    break;
+            }
+            #endregion
+        }
+
+        public async Task GetSentralAluno()
+        {
+            
+
         }
     }
 }
