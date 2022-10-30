cb8eaf3708ff97c294a7d9823f42a271c489e538
Felipe Muniz <felipemuniz_hub@outlook.com>
2022-10-29 23:45:00 -0300

    Inicio Implementação

--- AcessaEducation/EscolasIntegration/Uni9.cs
+++ AcessaEducation/EscolasIntegration/Uni9.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using PuppeteerSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,13 +10,23 @@ namespace AcessaEducation.EscolasIntegration
 {
     public class Uni9
     {
+        private string _loginURI = "https://aluno.uninove.br/seu/CENTRAL/aluno/";
+        private Page _currentPage;
+        private Browser _browser;
         public async Task AcessaUni9()
         {
-            uint ra = 2217105739;
-            string senha = "5Alc1cha@";
+            Console.WriteLine("=============================== Digite seu Ra =====================================");
+            string ra = Console.ReadLine();
+            Console.WriteLine("============================== Digite Sua Senha ====================================");
+            string senha = Console.ReadLine();
 
             #region Login
+            _browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = false
+            });
 
+            await _currentPage.GoToAsync(_loginURI);
             #endregion
         }
     }
