629a890b3ceb96f76fee39fa708a58c25588af8f
Felipe Muniz <felipemuniz_hub@outlook.com>
2022-10-29 23:29:54 -0300

    Adicionar arquivos de projeto.

---
+++ AcessaEducation/EscolasIntegration/Uni9.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcessaEducation.EscolasIntegration
+{
+    public class Uni9
+    {
+        public async Task AcessaUni9()
+        {
+            uint ra = 2217105739;
+            string senha = "5Alc1cha@";
+
+            #region Login
+
+            #endregion
+        }
+    }
+}
---
+++ AcessaEducation/Program.cs
@@ -0,0 +1,19 @@
+﻿using AcessaEducation.EscolasIntegration;
+Uni9 uni9 = new Uni9();
+
+Console.WriteLine("====================== Está Aplicação Integra Sistemas de Escolas ======================");
+Console.WriteLine("========================================================================================");
+
+Console.WriteLine("=============================== Escolha sua Escola =====================================");
+Console.WriteLine("========================================================================================");
+Console.WriteLine("1-) Universidade Nove de Julho");
+Console.WriteLine("========================================================================================");
+int escolaEscolha = int.Parse(Console.ReadLine());
+Console.WriteLine("========================================================================================");
+
+switch (escolaEscolha)
+{
+    case 1:
+        await uni9.AcessaUni9();
+        break;
+}
